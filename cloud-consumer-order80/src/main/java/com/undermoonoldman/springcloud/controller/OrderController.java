af2c78b09ba08d4574f88f316ff66d79d2f3b036
undermoonoldman <undermoonoldman@gmail.com>
2020-06-05 09:21:59 +0800

    订单中心更新，添加集群配置，新增负载均衡配置类

---
+++ cloud-consumer-order80/src/main/java/com/undermoonoldman/springcloud/controller/OrderController.java
@@ -0,0 +1,73 @@
+package com.undermoonoldman.springcloud.controller;
+
+import com.undermoonoldman.springcloud.entity.CommonResult;
+import com.undermoonoldman.springcloud.entity.Payment;
+import com.undermoonoldman.springcloud.lb.LoadBalancer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
+
+
+@RestController
+@Slf4j
+@RequestMapping("/consumer/payment")
+public class OrderController {
+    /**不能写死**/
+//    public static final  String PAYMENT_URL="http://localhost:8001";
+
+    public static final  String PAYMENT_URL="http://CLOUD-PAYMENT-SERVICE";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @PostMapping("/create")
+    public CommonResult<Payment> create(@RequestBody Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping("/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+    @GetMapping("/getForEntity/{id}")
+    public CommonResult<Payment> getForEntity(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+
+    /**
+     * 为了以上程序的正常执行：把自定义接口注释掉，不用自定义负载均衡算法，若想再次启动
+     * 请操作一下步骤：
+     *          1.注释掉@LoadBalanced（在config下面），放开下方注释，同时会导致上方不可用，因为找不到具体服务
+     */
+
+    /*@GetMapping(value = "/lb")
+    public String getPaymentLB(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if (instances ==null || instances.size()<=0){
+            return null;
+        }
+        *//*传入自己的*//*
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+        return restTemplate.getForObject(uri+"/payment/lb",String.class);
+    }*/
+
+}
---
+++ cloud-consumer-order80/src/main/java/com/undermoonoldman/springcloud/lb/MyLB.java
@@ -0,0 +1,34 @@
+package com.undermoonoldman.springcloud.lb;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+@Component
+public class MyLB implements LoadBalancer {
+
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public final int getAndIncrement(){
+        int current;
+        int next;
+        do{
+            current=this.atomicInteger.get();
+            /*2147483647:整型最大值*/
+            next= current >=2147483647 ? 0:current+1;
+        }while (!this.atomicInteger.compareAndSet(current,next));
+        System.out.println("******第几次访问next"+next);
+        return next;
+    }
+
+    //负载均衡算法：第几次请求%服务器总数量=实际访问。服务每次启动从1开始
+    @Override
+    public ServiceInstance instances(List<ServiceInstance> serviceInstances) {
+        int index= getAndIncrement() % serviceInstances.size();
+
+        return serviceInstances.get(index);
+    }
+}
